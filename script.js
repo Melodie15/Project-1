7a34b56508a32f9e5817772203b3c380ce76c45f
Jake Haberle <jake.haberle@gmail.com>
2020-08-08 12:00:53 -0600

    new css styles

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 8f5f76bfefe11208a23eba69140d7944f8927e15
-=======
-
->>>>>>> 8cf88bad4d1b51b680f15352052d8c633a3d4c36
 function displayDates() {
 	var dayfromselector = $("<select>")
 	dayfromselector.addClass("dayfromselector")
@@ -16,17 +8,10 @@ function displayDates() {
 		// Added a data-attribute
 		dayfromOption.attr("day", [i] );
 		if ( i < 10 ) {
-			dayfromOption.text("0"+jQuery.parseJSON(i)) 
+			dayfromOption.text("0"+$.parseJSON(i)) 
         } 
 		else {dayfromOption.text(i + 1)}
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
->>>>>>> 8f5f76bfefe11208a23eba69140d7944f8927e15
-=======
-
->>>>>>> 8cf88bad4d1b51b680f15352052d8c633a3d4c36
 		dayfromselector.append(dayfromOption)
 		$("#From-form").append(dayfromselector);
 	}
@@ -46,31 +31,17 @@ function displayDates() {
 	var dayUntilselector = $("<select>")
 	dayUntilselector.addClass("dayUntilSelector")
 	for (var i = 1; i < 31; i++){
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 		
->>>>>>> 8f5f76bfefe11208a23eba69140d7944f8927e15
-=======
-
->>>>>>> 8cf88bad4d1b51b680f15352052d8c633a3d4c36
 		var dayUntilOption = $("<option>")
 		// Adds a class of city to our button
 		dayUntilOption.addClass("until-day");
 		// Added a data-attribute
 		dayUntilOption.attr("day", [i] );
 		if ( i < 10 ) {
-			dayUntilOption.text("0"+jQuery.parseJSON(i)) 
+			dayUntilOption.text("0"+$.parseJSON(i)) 
         } 
 		else {dayUntilOption.text(i + 1)}
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 
->>>>>>> 8f5f76bfefe11208a23eba69140d7944f8927e15
-=======
-
->>>>>>> 8cf88bad4d1b51b680f15352052d8c633a3d4c36
 		dayUntilselector.append(dayUntilOption)
 		$("#until-form").append(dayUntilselector);
 	}
@@ -86,146 +57,6 @@ function displayDates() {
 		yearUntilOption.text(i + 2020)
 		yearUntilselector.append(yearUntilOption)
 		$("#until-form").append(yearUntilselector);
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 8cf88bad4d1b51b680f15352052d8c633a3d4c36
-	}
-}
-$("#add-cities").on("click", function(info) {
-	$("#hotel-view").empty()
-	$("#map").empty()
-	$("#attractions-view").empty()
-	$("#airBnB-view").empty()
-	$("#event-view").empty()
-	event.preventDefault();
-	var lats =[]
-	var logs = []
-	var names = []
-	var monthfromInput =$("#From-months").val()
-	var dayfromInput = $(".dayfromselector").val()
-	var yearfromInput = $(".yearfromselector").val()
-	var cityinput = $("#city-input").val().trim();
-	var monthUntilInput = $("#until-month").val()
-	var dayUntilInput = $(".dayUntilSelector").val()
-	var yearUntilInput =$(".yearUntilSelector").val()
-	var checkin = + yearfromInput + "-"+ monthfromInput + "-"+ dayfromInput 
-	var checkout = + yearUntilInput +"-"+ monthUntilInput+"-"+ dayUntilInput
-	tripadvisor()
-function tripadvisor(){ 
-	var tripAdvisorSettings = {
-	"async": false,
-	"crossDomain": true,
-	"url": "https://tripadvisor1.p.rapidapi.com/locations/search?location_id=10&limit=30&sort=relevance&offset=0&lang=en_US&currency=USD&units=km&query=" + cityinput,
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "tripadvisor1.p.rapidapi.com",
-		"x-rapidapi-key": "8fc8315f1amsh70aff4e1a3ba621p1d89e4jsn59ed596832c4"
-	}
-}
-$("#attractions-view").text("Attractions: ")
-$.ajax(tripAdvisorSettings).done(function (response) {
-	console.log(response)
-	console.log(response.data[0].result_object.location_id)
-	var tripAdvisorID = response.data[0].result_object.location_id
-	var lati = response.data[0].result_object.latitude
-	var lon = response.data[0].result_object.longitude
-	console.log(lati)
-	console.log(lon)
-	var AdvisorIDsettings = {
-		"async": false,
-		"crossDomain": true,
-		"url": "https://tripadvisor1.p.rapidapi.com/attractions/list?lang=en_US&currency=USD&sort=recommended&lunit=km&location_id="+tripAdvisorID,
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-host": "tripadvisor1.p.rapidapi.com",
-			"x-rapidapi-key": "8fc8315f1amsh70aff4e1a3ba621p1d89e4jsn59ed596832c4"
-		}
-	}
-	$.ajax(AdvisorIDsettings).done(function (response) {
-		console.log(response)
-		console.log(response.data[0].name)
-		console.log(response.data[0].website)
-		airBnbAPI()
-		googleMaps()
-		for (i = 0; i < 5 ; i++) {
-		var attractionDivEl = $("<div>");
-		attractionDivEl.addClass("attractions");
-		attractionDivEl.attr("data-set",[i]);
-		attractionDivEl.html("<a href="+ response.data[i].website+">" +response.data[i].name +'</a>');
-		var descriptionDivEl = $("<div>");
-		descriptionDivEl.addClass("description")
-		descriptionDivEl.text(response.data[i].description)
-		var image = response.data[i].photo.images.medium.url
-		var imageDivEl = $("<img>")
-		imageDivEl.attr("src", image)
-		imageDivEl.attr("width","20%")
-		attractionDivEl.append(descriptionDivEl,imageDivEl )
-		$("#attractions-view").append(attractionDivEl)	
-		var latitude = response.data[i].latitude
-		var longitude = response.data[i].longitude
-		var name = response.data[i].name
-		lats.push(latitude)
-		logs.push(longitude)
-		names.push(name)
-	}})
-	console.log(logs)
-	console.log(lats)
-	function googleMaps(){
-		var script = $('<script>');
-		script.attr("src",'https://maps.googleapis.com/maps/api/js?key=AIzaSyCCFEOkbkpCzlLVqGgBY4uflsf8ZXCPq-w&callback=initMap');
-		script.attr("defer", true)
-	// Attach your callback function to the `window` object
-	window.initMap = function() {
-		console.log(lati)
-	console.log(lon)
-	var centerLocation = {lat: Number(lati), lng: Number(lon)};
-	  var map = new google.maps.Map(
-		  document.getElementById("map"), {zoom: 13, center: centerLocation, gestureHandling: 'cooperative'});
-	  // The marker, positioned at Uluru
-	  var marker = new google.maps.Marker({position: centerLocation, map: map});
-	  for (i = 0; i < logs.length; i++) {
-		var infowindow = new google.maps.InfoWindow({
-			content: contentString
-		});
-		var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(Number(lats[i]), Number(logs[i])),
-			map: map,
-			title: names[i]
-		  });
-		  var contentString = '<div id="content">'+
-      	'<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" class="firstHeading">'+names[i]+'</h1>'+
-      '<div id="bodyContent">'+ names[i]+
-      '</div>'+
-	  '</div>';
-		marker.addListener('click', function(e) {
-			infowindow.open(map, marker);
-		  })
-	  }
-	};
-	console.log(script[0])
-	// Append the 'script' element to 'head'
-	document.head.append(script[0]);
-	}
-	function airBnbAPI(){
-		var settings = {
-		"async": false,
-		"crossDomain": true,
-		"url": "https://airbnb-com.p.rapidapi.com/listings/nearby/"+lati+"/"+lon+"?min_bathrooms=0&check_out=" + checkout+"&hotel_room=true&max_guests=1&check_in=" + checkin + "&private_room=true&min_bedrooms=0&offset=0&entire_home=true&min_price=0&max_price=5000&min_beds=0&radius=5&shared_room=true",
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-host": "airbnb-com.p.rapidapi.com",
-			"x-rapidapi-key": "8fc8315f1amsh70aff4e1a3ba621p1d89e4jsn59ed596832c4"
-		}
-	}
-<<<<<<< HEAD
-=======
-=======
-
->>>>>>> 8cf88bad4d1b51b680f15352052d8c633a3d4c36
 	}
 }
 
@@ -251,58 +82,112 @@ $("#add-cities").on("click", function(info) {
 	var yearUntilInput =$(".yearUntilSelector").val()
 	var checkin = + yearfromInput + "-"+ monthfromInput + "-"+ dayfromInput 
 	var checkout = + yearUntilInput +"-"+ monthUntilInput+"-"+ dayUntilInput
+
 	tripadvisor()
+	hotelsAPI()
 
-
-
-function tripadvisor(){ 
+	$(".search").toggle("slow")
+	var toggleFormButton =$("<button>")
+	toggleFormButton.attr("id", "toggleFormButton")
+	toggleFormButton.addClass("button is-small is-primary is-rounded")
+	$("#searchDiv").empty()
+	$("#searchDiv").prepend(toggleFormButton)
+	toggleFormButton.text("Search Form")
+	$("#toggleFormButton").click(function(){
+		$(".search").toggle("slow", function(){
+			if($(this).is(":visible")){
+				$("#toggleFormButton").text("Hide Search");
+			} else {
+				$("#toggleFormButton").text("Search Another City")
+			};
+		});
+	});
+	
+	function tripadvisor(){ 
 	var tripAdvisorSettings = {
-	"async": false,
 	"crossDomain": true,
 	"url": "https://tripadvisor1.p.rapidapi.com/locations/search?location_id=10&limit=30&sort=relevance&offset=0&lang=en_US&currency=USD&units=km&query=" + cityinput,
 	"method": "GET",
 	"headers": {
 		"x-rapidapi-host": "tripadvisor1.p.rapidapi.com",
-		"x-rapidapi-key": "8fc8315f1amsh70aff4e1a3ba621p1d89e4jsn59ed596832c4"
-	}
-}
-$("#attractions-view").text("Attractions: ")
-$.ajax(tripAdvisorSettings).then(function (response) {
+		"x-rapidapi-key": "d4d0ed6519msh33b01fff60e8af4p1055fdjsna4a8a8c4be3f"
+	}}
+
+	$.ajax(tripAdvisorSettings).then(function (response) {
 	var tripAdvisorID = response.data[0].result_object.location_id
 	var lati = response.data[0].result_object.latitude
 	var lon = response.data[0].result_object.longitude
 	var AdvisorIDsettings = {
-		"async": true,
 		"crossDomain": true,
 		"url": "https://tripadvisor1.p.rapidapi.com/attractions/list?lang=en_US&currency=USD&sort=recommended&lunit=km&location_id="+tripAdvisorID,
 		"method": "GET",
 		"headers": {
 			"x-rapidapi-host": "tripadvisor1.p.rapidapi.com",
-			"x-rapidapi-key": "8fc8315f1amsh70aff4e1a3ba621p1d89e4jsn59ed596832c4"
-		}
-	}
-	
+			"x-rapidapi-key": "d4d0ed6519msh33b01fff60e8af4p1055fdjsna4a8a8c4be3f"
+		}}
 	$.ajax(AdvisorIDsettings).done(function (response) {
 		console.log(response)
 		airBnbAPI()
 		googleMaps()
 		
-		
+		var header = $("<div>")
+		header.text("Attractions")
+		header.addClass("attractionheader header title is-3 has-text-centered")
+		$("#attractions-view").append(header)
+	
+	var toggleAttractionButton =$("<button>")
+	toggleAttractionButton.attr("id", "toggleAttractionButton")
+	toggleAttractionButton.addClass(" button is-small is-primary is-rounded")
+	$(".attractionheader").append(toggleAttractionButton)
+	toggleAttractionButton.text("Show")
+	$("#toggleAttractionButton").click(function(){
+		$(".attractions").toggle("slow", function(){
+			if($(this).is(":visible")){
+				$("#toggleAttractionButton").text("Hide");
+			} else {
+				$("#toggleAttractionButton").text("Show")
+			}
+		})
+	
+	})
 		for (i = 0; i < 5 ; i++) {
 		var attractionDivEl = $("<div>");
-		attractionDivEl.addClass("attractions");
+		attractionDivEl.addClass("attractions card column");
+		var div2 = $("<div>");
+		div2.addClass("card-content")
 		attractionDivEl.attr("data-set",[i]);
-		attractionDivEl.html("<a href="+ response.data[i].website+">" +response.data[i].name +'</a>');
-		var descriptionDivEl = $("<div>");
+		attractionDivEl.append(div2)
+		var p1 = $("<p>");
+		p1.addClass("title  has-text-centered")
+		p1.text(response.data[i].name)
+		var descriptionDivEl = $("<p>");
 		descriptionDivEl.addClass("description")
 		descriptionDivEl.text(response.data[i].description)
+		div2.append(p1,descriptionDivEl)
+		var footer = $("<footer>")
+		footer.addClass("card-footer")
+		attractionDivEl.append(footer)
 		
+		var p1footer = $("<p>")
+		p1footer.addClass("card-footer-item subtitle")
+		var span1 = $("<span>")
+		span1.html("<a href="+ response.data[i].website+">" +response.data[i].name + " Website" + '</a>')
+		p1footer.append(span1)
+
+		var p2footer = $("<p>")
+		p2footer.addClass("card-footer-item")
+		var span2 = $("<span>")
+		p2footer.append(span2)
 		var image = response.data[i].photo.images.small.url
 		var imageDivEl = $("<img>")
 		imageDivEl.attr("src", image)
-		imageDivEl.attr("width","20%")
-		attractionDivEl.append(descriptionDivEl,imageDivEl )
+		imageDivEl.attr("width","150px")
+		span2.append(imageDivEl)
+
+		footer.append(p1footer,p2footer)
+	
 		$("#attractions-view").append(attractionDivEl)	
+
 		var latitude = response.data[i].latitude
 		var longitude = response.data[i].longitude
 		var name = response.data[i].name
@@ -314,7 +199,11 @@ $.ajax(tripAdvisorSettings).then(function (response) {
 		descriptions.push(description)
 		images.push(image)
 		urls.push(url)
-	}})
+		};
+		$(".attractions").hide()
+	});
+	
+
 	function googleMaps(){
 		var script = $('<script>');
 		script.attr("src",'https://maps.googleapis.com/maps/api/js?key=AIzaSyCCFEOkbkpCzlLVqGgBY4uflsf8ZXCPq-w&callback=initMap');
@@ -352,83 +241,127 @@ $.ajax(tripAdvisorSettings).then(function (response) {
 	  }
 	};
 
-	document.head.append(script[0]);
-	}
+	document.head.prepend(script[0]);
+		};
 
 	function airBnbAPI(){
 		var settings = {
-		"async": false,
 		"crossDomain": true,
 		"url": "https://airbnb-com.p.rapidapi.com/listings/nearby/"+lati+"/"+lon+"?min_bathrooms=0&check_out=" + checkout+"&hotel_room=true&max_guests=1&check_in=" + checkin + "&private_room=true&min_bedrooms=0&offset=0&entire_home=true&min_price=0&max_price=5000&min_beds=0&radius=5&shared_room=true",
 		"method": "GET",
 		"headers": {
 			"x-rapidapi-host": "airbnb-com.p.rapidapi.com",
-			"x-rapidapi-key": "8fc8315f1amsh70aff4e1a3ba621p1d89e4jsn59ed596832c4"
+			"x-rapidapi-key": "d4d0ed6519msh33b01fff60e8af4p1055fdjsna4a8a8c4be3f"
 		}
 	}
 	
-<<<<<<< HEAD
->>>>>>> 8f5f76bfefe11208a23eba69140d7944f8927e15
-=======
-
->>>>>>> 8cf88bad4d1b51b680f15352052d8c633a3d4c36
 	$.ajax(settings).done(function (response) {
 		console.log(response)
-		$("#airBnB-view").text("AirBnb:")
+		$("#airBnB-view").text("AirBnb")
 		for (i = 0; i < 5 ; i++) {
 			var AirbnbDivEL = $("<div>")
 			var AirID = response.listings[i].listing.id
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 8cf88bad4d1b51b680f15352052d8c633a3d4c36
-			console.log(response.listings[0].listing.id)
-			console.log(response.listings[0].listing.room_and_property_type)
-			console.log(response.listings[0].pricing_quote.price_string)
 			AirbnbDivEL.addClass("airbnbDiv")
 			AirbnbDivEL.html('<a href='+"https://www.airbnb.com/rooms/"+ AirID +"?adults=1&location="+ cityinput +"&check_in=" + checkin + "&" + "check_out=" + checkout + "&display_extensions%5B%5D=MONTHLY_STAYS&source_impression_id=p3_1596475569_Ye1hL0KJyqYINSTH>" + response.listings[i].listing.room_and_property_type + "-  " + response.listings[i].pricing_quote.price_string + '</a>')
-			console.log(response)
-<<<<<<< HEAD
-=======
-			AirbnbDivEL.addClass("airbnbDiv")
-			AirbnbDivEL.html('<a href='+"https://www.airbnb.com/rooms/"+ AirID +"?adults=1&location="+ cityinput +"&check_in=" + checkin + "&" + "check_out=" + checkout + "&display_extensions%5B%5D=MONTHLY_STAYS&source_impression_id=p3_1596475569_Ye1hL0KJyqYINSTH>" + response.listings[i].listing.room_and_property_type + "-  " + response.listings[i].pricing_quote.price_string + '</a>')
-
->>>>>>> 8f5f76bfefe11208a23eba69140d7944f8927e15
-=======
-
-			AirbnbDivEL.addClass("airbnbDiv")
-			AirbnbDivEL.html('<a href='+"https://www.airbnb.com/rooms/"+ AirID +"?adults=1&location="+ cityinput +"&check_in=" + checkin + "&" + "check_out=" + checkout + "&display_extensions%5B%5D=MONTHLY_STAYS&source_impression_id=p3_1596475569_Ye1hL0KJyqYINSTH>" + response.listings[i].listing.room_and_property_type + "-  " + response.listings[i].pricing_quote.price_string + '</a>')
-
-
->>>>>>> 8cf88bad4d1b51b680f15352052d8c633a3d4c36
 			$("#airBnB-view").append(AirbnbDivEL)
+			var image = response.listings[i].listing.picture_url
+			var imageDivEl = $("<img>")
+			imageDivEl.addClass("airBnb-image")
+			imageDivEl.attr("src", image)
+			imageDivEl.attr("width","20%")
+			AirbnbDivEL.prepend(imageDivEl)
 		}
 	});
-	};
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+		};
 
->>>>>>> 8cf88bad4d1b51b680f15352052d8c633a3d4c36
+	});
+	};	
+
+	function hotelsAPI(){
+		var hotelheader = $("<div>")
+		hotelheader.text("Hotels")
+		hotelheader.addClass("header title is-3 hotel-header has-text-centered")
+		$("#hotel-view").append(hotelheader)
+
+		var settings = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://hotels4.p.rapidapi.com/locations/search?locale=en_US&query=" + cityinput,
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "hotels4.p.rapidapi.com",
+			"x-rapidapi-key": "d4d0ed6519msh33b01fff60e8af4p1055fdjsna4a8a8c4be3f"
+		}};
+		$.ajax(settings).done(function (response) {
+
+	var toggleHotelButton =$("<button>")
+	toggleHotelButton.attr("id", "toggleHotelButton")
+	toggleHotelButton.addClass(" button is-small is-primary is-rounded")
+	$(".hotel-header").append(toggleHotelButton)
+	toggleHotelButton.text("Show")
+	$("#toggleHotelButton").click(function(){
+		$(".hotel-div").toggle("slow", function(){
+			if($(this).is(":visible")){
+				$("#toggleHotelButton").text("Hide");
+			} else {
+				$("#toggleHotelButton").text("Show")
+			}
+		})
+	
+	})	
+		console.log(response);
+		console.log(response.suggestions[0].entities[0].destinationId)
+		var city = response.suggestions[0].entities[0].destinationId
+		var settings2 = {
+		"async": true,
+		"crossDomain": true,
+		"url": "https://hotels4.p.rapidapi.com/properties/list?currency=USD&locale=en_US&sortOrder=PRICE&destinationId=" + city + "&pageNumber=1&checkIn=" + yearfromInput + "-"+ monthfromInput + "-"+ dayfromInput + "&checkOut=" + yearUntilInput +"-"+ monthUntilInput+"-"+ dayUntilInput+"&pageSize=25&adults1=1",
+		"method": "GET",
+		"headers": {
+			"x-rapidapi-host": "hotels4.p.rapidapi.com",
+			"x-rapidapi-key": "d4d0ed6519msh33b01fff60e8af4p1055fdjsna4a8a8c4be3f"
+		}
+		}
+		$.ajax(settings2).done(function (response) {
+		console.log(response)
+		console.log(response.data.body.searchResults.results)
+		var searchResults = response.data.body.searchResults.results
+			for (var i = 0; i < 5; i++) {
+				var hotelID = searchResults[i].supplierHotelId
+				
+				var hotelResult = $("<div>");
+				hotelResult.addClass("hotel");
+				hotelResult.attr("data-name", searchResults[i].name)
+				hotelResult.html("<a href=https://www.expedia.com/h" + hotelID +".Hotel-Information?chkin=" + checkin +'&chkout=' +checkout+">"+searchResults[i].name +'</a>')
+				var rateDiv = $("<div>")
+				console.log (searchResults[0].ratePlan.price.current)
+				var rate = searchResults[i].ratePlan.price.current
+				rateDiv.text("Daily Rates: " + rate)
+				
+				var divImg = $("<div>")
+				divImg.addClass("level-left hotel-div")
+				var image = searchResults[i].thumbnailUrl
+				var imageDivEl = $("<img>")
+				imageDivEl.attr("src", image)
+				imageDivEl.attr("width","150px")
+				
+	
+
+				var reviewDiv = $("<div>")
+				console.log (searchResults[0].ratePlan.price.current)
+				var rate = searchResults[0].guestReviews.rating
+				reviewDiv.text("Guest Reviews: " + rate +"/10")
+				hotelResult.append(rateDiv, reviewDiv)
+				divImg.append(imageDivEl, hotelResult)
+				$("#hotel-view").append(divImg); 
+			}
+			$(".hotel-div").hide()
+		})
+	}
+	)};
+
 });
-}	
-});
+
+
+
 displayDates()
-<<<<<<< HEAD
-=======
-=======
-
->>>>>>> 8cf88bad4d1b51b680f15352052d8c633a3d4c36
-
-});
-}	
-});
-
-
-displayDates()
-<<<<<<< HEAD
->>>>>>> 8f5f76bfefe11208a23eba69140d7944f8927e15
-=======
-
->>>>>>> 8cf88bad4d1b51b680f15352052d8c633a3d4c36
